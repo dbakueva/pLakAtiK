0b89182fe2ea872c1a51f7fc7059480069ab9e72
dbakueva <44946675+dbakueva@users.noreply.github.com>
2019-06-14 01:48:47 +0300

    Add files via upload

---
+++ project.r
@@ -0,0 +1,59 @@
+library(tidyverse) #подключаем библиотеки
+library(janitor)
+
+library(showtext)
+showtext_auto()
+
+path <- 'D:/Master/kekonom/project/'  #Задаем путь
+
+#График общей смертности
+
+rus_total <- read_table(paste0(path, "Deaths_1x1.txt"), na = ".")   #Загружаем базу данных
+rus_total$Age <- as.integer(recode(rus_total$Age, "110+" = "110"))
+
+p_total <- ggplot(rus_total, aes(x = Year, y = Age, fill = ntile(Total, 100)))   #График
+p_total_out <- p_total + geom_raster() +
+  scale_fill_viridis_c(option = "A") +
+  guides(fill = guide_legend(label.position = "bottom", title.position = "top")) +
+  labs(x = "Year", y = "Age", fill = "Total Death Rate Percentile",
+       title = "Total Mortality in Russia, 1959-2014",
+       caption = "Data: Human Mortality Database.") +
+  theme(legend.position = "top",
+        legend.title = element_text(size = 20))
+
+p_total_out
+
+ggsave("D:/Master/kekonom/project/rus_total.png", p_total_out, height = 4, width = 6)
+
+#График отношения смертей мужчин к женщинам
+
+p_rel <- ggplot(rus_total, aes(x = Year, y = Age, fill = ntile(Male / Female, 100)))
+p_rel_out <- p_rel + geom_raster() +
+  scale_fill_viridis_c(option = "A") +
+  guides(fill = guide_legend(label.position = "bottom", title.position = "top")) +
+  labs(x = "Year", y = "Age", fill = "Percentile of Male to Female Death Rates",
+       title = "Ratio of Male to Female Mortality Rates in Russia, 1959-2014") +
+  theme(legend.position = "top",
+        legend.title = element_text(size = 20))
+
+p_rel_out
+
+ggsave("D:/Master/kekonom/project/rus_rel.png", p_rel_out, height = 4, width = 6)
+
+#График склонность к риску
+rus_risk <- read_table(paste0(path, "Exposures_1x1.txt"), na = ".")   #Загружаем другую базу данных
+rus_risk$Age <- as.integer(recode(rus_risk$Age, "110+" = "110"))
+
+p_risk <- ggplot(subset(rus_risk, Age < 101), aes(x = Year, y = Age, fill = ntile(Total, 100)))
+p_risk_out <- p_risk + geom_raster() +
+  scale_fill_viridis_c(option = "A", direction = -1) +
+  scale_x_continuous(breaks = seq(1959, 2014, by = 5)) +
+  guides(fill = guide_legend(nrow = 1, title.position = "top", label.position = "bottom")) +
+  ylim(c(0, 100)) +
+  labs(x = "Year", y = "Age", fill = "Exposure-to-risk Percentile",
+       title = "Total exposure to risk in Russia, 1959-2014") +
+  theme(legend.position = "top",
+        legend.title = element_text(size = 20))
+
+p_risk_out
+ggsave("D:/Master/kekonom/project/rus_exp.png", p_risk_out, height = 4, width = 6)
